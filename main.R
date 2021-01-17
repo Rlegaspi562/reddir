b2a231e6a04c8d40e7581bf7d0e426adf04061eb
Rumil Legaspi <59223473+Rlegaspi562@users.noreply.github.com>
2021-01-16 19:15:19 -0800

    Add files via upload

--- main.R
+++ main.R
@@ -1,13 +1,24 @@
+# Loading in redditr package, check description for more info
 library(redditr)
 
 
-
+# This code chunk lets me parameterize
 crawl_reddit(n_steps = 5,
              name ='politics',
-             type = 'new',
+             type = 'new', # what is this again?
              limit = 10,
              file_name = 'reddit_data.csv',
              sleep_time = 3)
 
-
+#reads the output into csv and stores it inside response
 response <- read_csv('reddit_data.csv')
+
+
+#This code groups by to isolate distinct observations by submission key
+response <-
+  response %>%
+  group_by(submission_key) %>%
+  mutate(
+    id = row_number()
+  )
+
